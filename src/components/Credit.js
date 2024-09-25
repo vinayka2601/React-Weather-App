6b38f466c5fa5b360e34d3e0548371f18c25633c
vinayka2601 <vinaykaawasthi26@gmail.com>
2024-09-25 23:52:19 +0530

    first commit

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+    {/* https://www.shecodes.io/graduates/73033-chaimae-lamirine */}
+      <a href=""
+        > </a
+      >, <a href=""></a> 
+    </p>
+  </div>
+  );
+}
