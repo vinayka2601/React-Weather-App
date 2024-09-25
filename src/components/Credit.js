712d8eff6fcc9d326eb8410d7b4c5e3dd3811a9d
vinayka2601 <vinaykaawasthi26@gmail.com>
2024-09-26 00:02:52 +0530

    githubID

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -7,7 +7,7 @@ export default function Credit() {
     {/* https://www.shecodes.io/graduates/73033-chaimae-lamirine */}
       <a href=""
         > </a
-      >, <a href=""></a> 
+      >, <a href="https://github.com/vinayka2601/React-Weather-App.git"></a> 
     </p>
   </div>
   );
